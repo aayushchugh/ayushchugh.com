652b9bb0a1260b9cf97b4f136dc715eb9db416ed
Ayush Chugh <hey@ayushchugh.com>
2024-09-13 22:21:22 +0530

    refactor: clean up formatting and improve readability in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,32 @@
-import { sql } from '@vercel/postgres';
-
 const nextConfig = {
-  experimental: {
-    ppr: true,
-    useLightningcss: true,
-  },
-  async redirects() {
-    if (!process.env.POSTGRES_URL) {
-      return [];
-    }
+	experimental: {
+		ppr: true,
+		useLightningcss: true,
+	},
+	async redirects() {
+		if (!process.env.POSTGRES_URL) {
+			return [];
+		}
 
-    const { rows: redirects } = await sql`
+		const { rows: redirects } = await sql`
       SELECT source, destination, permanent
       FROM redirects;
     `;
 
-    return redirects.map(({ source, destination, permanent }) => ({
-      source,
-      destination,
-      permanent: !!permanent,
-    }));
-  },
-  headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: securityHeaders,
-      },
-    ];
-  },
+		return redirects.map(({ source, destination, permanent }) => ({
+			source,
+			destination,
+			permanent: !!permanent,
+		}));
+	},
+	headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: securityHeaders,
+			},
+		];
+	},
 };
 
 const ContentSecurityPolicy = `
@@ -42,34 +40,34 @@ const ContentSecurityPolicy = `
 `;
 
 const securityHeaders = [
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, ''),
-  },
-  {
-    key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin',
-  },
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY',
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains; preload',
-  },
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
-  },
+	{
+		key: "Content-Security-Policy",
+		value: ContentSecurityPolicy.replace(/\n/g, ""),
+	},
+	{
+		key: "Referrer-Policy",
+		value: "origin-when-cross-origin",
+	},
+	{
+		key: "X-Frame-Options",
+		value: "DENY",
+	},
+	{
+		key: "X-Content-Type-Options",
+		value: "nosniff",
+	},
+	{
+		key: "X-DNS-Prefetch-Control",
+		value: "on",
+	},
+	{
+		key: "Strict-Transport-Security",
+		value: "max-age=31536000; includeSubDomains; preload",
+	},
+	{
+		key: "Permissions-Policy",
+		value: "camera=(), microphone=(), geolocation=()",
+	},
 ];
 
 export default nextConfig;
