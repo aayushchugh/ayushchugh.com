384d9235636561fd910e137ae0eb46a36cb05f0e
Ayush Chugh <hey@ayushchugh.com>
2024-09-14 08:44:44 +0530

    fix: google analytics cors error

--- next.config.mjs
+++ next.config.mjs
@@ -3,30 +3,30 @@ const nextConfig = {
 		ppr: true,
 		useLightningcss: true,
 	},
-	async redirects() {
-		if (!process.env.POSTGRES_URL) {
-			return [];
-		}
+	// async redirects() {
+	// 	if (!process.env.POSTGRES_URL) {
+	// 		return [];
+	// 	}
 
-		const { rows: redirects } = await sql`
-      SELECT source, destination, permanent
-      FROM redirects;
-    `;
+	// 	const { rows: redirects } = await sql`
+	//     SELECT source, destination, permanent
+	//     FROM redirects;
+	//   `;
 
-		return redirects.map(({ source, destination, permanent }) => ({
-			source,
-			destination,
-			permanent: !!permanent,
-		}));
-	},
-	headers() {
-		return [
-			{
-				source: "/(.*)",
-				headers: securityHeaders,
-			},
-		];
-	},
+	// 	return redirects.map(({ source, destination, permanent }) => ({
+	// 		source,
+	// 		destination,
+	// 		permanent: !!permanent,
+	// 	}));
+	// },
+	// headers() {
+	// 	return [
+	// 		{
+	// 			source: "/(.*)",
+	// 			headers: securityHeaders,
+	// 		},
+	// 	];
+	// },
 };
 
 const ContentSecurityPolicy = `
@@ -39,35 +39,35 @@ const ContentSecurityPolicy = `
     font-src 'self' data:;
 `;
 
-const securityHeaders = [
-	{
-		key: "Content-Security-Policy",
-		value: ContentSecurityPolicy.replace(/\n/g, ""),
-	},
-	{
-		key: "Referrer-Policy",
-		value: "origin-when-cross-origin",
-	},
-	{
-		key: "X-Frame-Options",
-		value: "DENY",
-	},
-	{
-		key: "X-Content-Type-Options",
-		value: "nosniff",
-	},
-	{
-		key: "X-DNS-Prefetch-Control",
-		value: "on",
-	},
-	{
-		key: "Strict-Transport-Security",
-		value: "max-age=31536000; includeSubDomains; preload",
-	},
-	{
-		key: "Permissions-Policy",
-		value: "camera=(), microphone=(), geolocation=()",
-	},
-];
+// const securityHeaders = [
+// 	{
+// 		key: "Content-Security-Policy",
+// 		value: ContentSecurityPolicy.replace(/\n/g, ""),
+// 	},
+// 	// {
+// 	// 	key: "Referrer-Policy",
+// 	// 	value: "origin-when-cross-origin",
+// 	// },
+// 	{
+// 		key: "X-Frame-Options",
+// 		value: "DENY",
+// 	},
+// 	{
+// 		key: "X-Content-Type-Options",
+// 		value: "nosniff",
+// 	},
+// 	{
+// 		key: "X-DNS-Prefetch-Control",
+// 		value: "on",
+// 	},
+// 	{
+// 		key: "Strict-Transport-Security",
+// 		value: "max-age=31536000; includeSubDomains; preload",
+// 	},
+// 	{
+// 		key: "Permissions-Policy",
+// 		value: "camera=(), microphone=(), geolocation=()",
+// 	},
+// ];
 
 export default nextConfig;
