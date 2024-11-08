b4807e24df9552926e2f0b5b1d98f3bc71e1f110
Ayush Chugh <hey@ayushchugh.com>
2024-11-09 00:19:44 +0530

    feat: add footer component with social links and update contact page for email contact

---
+++ app/components/footer.tsx
@@ -0,0 +1,55 @@
+import {
+  EnvelopeSimple,
+  GithubLogo,
+  LinkedinLogo,
+  XLogo,
+} from "@phosphor-icons/react/dist/ssr";
+import type { FC } from "react";
+
+const socials = [
+  {
+    name: "Email",
+    url: "mailto:hey@ayushchugh.com",
+    logo: EnvelopeSimple,
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/aayushchugh",
+    logo: GithubLogo,
+  },
+  {
+    name: "X",
+    url: "https://x.com/aayushchugh_x",
+    logo: XLogo,
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/aayushchugh/",
+    logo: LinkedinLogo,
+  },
+];
+
+const Footer: FC = () => {
+  return (
+    <>
+      <footer className="mt-8 flex justify-center">
+        <ul className="flex gap-4">
+          {socials.map((social, index) => (
+            <li key={index}>
+              <a
+                href={social.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block p-2 hover:bg-gray-200 dark:hover:bg-gray-800 rounded-full"
+              >
+                <social.logo size={24} />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,94 +7,96 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 
 import { PreloadResources } from "./preload";
 import Cmdk from "./components/CmdK";
+import Footer from "./components/footer";
 
 export const metadata: Metadata = {
-	metadataBase: new URL("https://ayushchugh.com/"),
-	title: {
-		default: "Ayush Chugh",
-		template: "%s | Ayush Chugh",
-	},
-	description:
-		"Passionate full stack developer from India. On the way to master programming",
-	openGraph: {
-		title: "Ayush Chugh",
-		description:
-			"Passionate full stack developer from India. On the way to master programming.",
-		url: "https://ayushchugh.com/",
-		siteName: "Ayush Chugh's Portfolio",
-		locale: "en_US",
-		type: "website",
-		images: [
-			{
-				url: "/ogs/ogs-bg.png",
-				width: 1200,
-				height: 630,
-				alt: "Ayush Chugh Full Stack Developer",
-			},
-		],
-	},
-	robots: {
-		index: true,
-		follow: true,
-		googleBot: {
-			index: true,
-			follow: true,
-			"max-video-preview": -1,
-			"max-image-preview": "large",
-			"max-snippet": -1,
-		},
-	},
-	twitter: {
-		title: "Ayush Chugh",
-		card: "summary_large_image",
-		creator: "@aayushchugh_x",
-		creatorId: "@aayushchugh_x",
-		site: "@aayushchugh_x",
-		siteId: "@aayushchugh_x",
-		description:
-			"Passionate full stack developer from India. On the way to master programming.",
-		images: ["/ogs/ogs-bg.png"],
-	},
-	icons: {
-		shortcut: "/favicon.png",
-	},
+  metadataBase: new URL("https://ayushchugh.com/"),
+  title: {
+    default: "Ayush Chugh",
+    template: "%s | Ayush Chugh",
+  },
+  description:
+    "Passionate full stack developer from India. On the way to master programming",
+  openGraph: {
+    title: "Ayush Chugh",
+    description:
+      "Passionate full stack developer from India. On the way to master programming.",
+    url: "https://ayushchugh.com/",
+    siteName: "Ayush Chugh's Portfolio",
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: "/ogs/ogs-bg.png",
+        width: 1200,
+        height: 630,
+        alt: "Ayush Chugh Full Stack Developer",
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "Ayush Chugh",
+    card: "summary_large_image",
+    creator: "@aayushchugh_x",
+    creatorId: "@aayushchugh_x",
+    site: "@aayushchugh_x",
+    siteId: "@aayushchugh_x",
+    description:
+      "Passionate full stack developer from India. On the way to master programming.",
+    images: ["/ogs/ogs-bg.png"],
+  },
+  icons: {
+    shortcut: "/favicon.png",
+  },
 };
 
 const cx = (...classes) => classes.filter(Boolean).join(" ");
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html
-			lang="en"
-			className={cx(
-				"text-black bg-white dark:text-white dark:bg-[#111010]",
-				GeistSans.variable,
-				GeistMono.variable
-			)}
-		>
-			<head>
-				<script
-					async
-					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
-				></script>
-			</head>
-			<body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
-				<main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-					<Cmdk />
-					<Navbar />
-					{children}
+  return (
+    <html
+      lang="en"
+      className={cx(
+        "text-black bg-white dark:text-white dark:bg-[#111010]",
+        GeistSans.variable,
+        GeistMono.variable,
+      )}
+    >
+      <head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+        ></script>
+      </head>
+      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          <Cmdk />
+          <Navbar />
+          {children}
+          <Footer />
 
-					<PreloadResources />
-				</main>
-			</body>
-			{process.env.GOOGLE_ANALYTICS_ID &&
-				process.env.NODE_ENV === "production" && (
-					<GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID} />
-				)}
-		</html>
-	);
+          <PreloadResources />
+        </main>
+      </body>
+      {process.env.GOOGLE_ANALYTICS_ID &&
+        process.env.NODE_ENV === "production" && (
+          <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID} />
+        )}
+    </html>
+  );
 }
